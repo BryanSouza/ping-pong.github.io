997c54c59b48556b6435bb8aade864f0228d76c2
Bryan Souza <bryansouza_01@outlook.com>
2021-05-08 21:40:13 -0300

    add animation

--- index.js
+++ index.js
@@ -60,10 +60,6 @@ class Vector {
 }
 
 class Impulse extends Vector {
-    getForce() {
-        return super.getMagnitude();
-    }
-
     constructor(direction, sense, force, time) {
         super(force);
         super.setDirectionAndSense(direction, sense);
@@ -72,22 +68,36 @@ class Impulse extends Vector {
 }
 
 class PhysicalObject {
-    static #gravity = 9.8; //Earth's Gravity
-    static #frictions_coefficient = 0.2; //Friction's coefficient
+    #gravity = 9.8; //Earth's Gravity
+    #frictions_coefficient = 0.2; //Friction's coefficient
+    #initialVelocity = 0;
+    #raf;
 
-    static move(impulse) {
+    move(impulse, context) {
         let normalForce = this.weight * this.#gravity;
+        let finalVelocity;
+
         switch(impulse.getDirection()) {
             case Direction.Horizontal:
                 let friction = normalForce * this.#frictions_coefficient;
-                let resultantForce = impulse.getForce() - friction;
+                let resultantForce = impulse.getMagnitude() - friction;
                 if(resultantForce > 0) {
+
+                    finalVelocity = ( (impulse.getMagnitude() * impulse.time) / this.weight ) + this.#initialVelocity;
+
+                    let object = context.getWidthAndHeight();
                     switch(impulse.getSense()) {
                         case Sense.Left:
-                            console.log("left");
+                            if( !(this.currentPosition.X - finalVelocity < 5) ) {
+                                this.currentPosition.X -= finalVelocity;
+                                this.#raf = window.requestAnimationFrame(()=> { context.drawRect(grandstand.context, this.currentPosition) });
+                            }
                             break;
                         default:
-                            console.log("right");
+                            if( !(this.currentPosition.X + finalVelocity > grandstand.width - object.width) ) {
+                                this.currentPosition.X += finalVelocity;
+                                this.#raf = window.requestAnimationFrame(()=> { context.drawRect(grandstand.context, this.currentPosition) });
+                            }
                             break;
                     }
                 } else {
@@ -103,6 +113,15 @@ class PhysicalObject {
                         console.log("down");
                         break;
                 }
+        }
+
+    }
+
+    clearAnimationFrame() {
+        if(this.#raf) {
+            console.log('canceling animation frame: ', this.#raf);
+            window.cancelAnimationFrame(this.#raf);
+            this.#raf = null;
         }
     }
 
@@ -127,8 +146,20 @@ class Rectangle {
         }
     }
 
-    draw(callback){
-        return callback(this.#width, this.#height, this.#color);
+    getWidthAndHeight() {
+        return {
+            height: this.#height,
+            width: this.#width
+        }
+    }
+
+    drawRect(context, position) {
+        context.clearRect(0, 0, 600, 700);
+        context.beginPath();
+        context.rect(position.X, position.Y, this.#width, this.#height);
+        context.fillStyle = this.#color;
+        context.fill();
+        context.closePath();
     }
 
     constructor(rectangle) {
@@ -139,24 +170,38 @@ class Rectangle {
     }
 }
 
-class Paddle extends Rectangle {
+class Racket extends Rectangle {
     #physicalObject;
 
-    static #move(impulse) {
-        this.#physicalObject.move(impulse);
+    #move(impulse) {
+        this.#physicalObject.move(impulse, this);
     }
 
-    build() {
-        this.draw((width, height, color) => {
-            console.log('width: ', width);
-            console.log('height: ', height);
-            console.log('color: ', color);
-        });
+    moveLeft() {
+        let leftImpulse = new Impulse(Direction.Horizontal, Sense.Left, 50, 5);
+        this.#move(leftImpulse);
     }
 
-    constructor(paddle) {
-        super(paddle.rectangle);
-        this.#physicalObject = new PhysicalObject(paddle.position, paddle.weight);
+    moveRight() {
+        let rightImpulse = new Impulse(Direction.Horizontal, Sense.Right, 50, 5);
+        this.#move(rightImpulse);
+    }
+
+    clearAnimation() {
+        this.#physicalObject.clearAnimationFrame();
+    }
+
+    constructor(racket) {
+        super(racket.rectangle);
+        this.#physicalObject = new PhysicalObject(racket.position, racket.weight);
+        this.drawRect(grandstand.context, this.#physicalObject.currentPosition)
+    }
+}
+
+class Grandstand extends HTMLCanvasElement {
+    constructor() {
+        super();
+        this.context = this.getContext("2d");
     }
 }
 
@@ -169,16 +214,52 @@ const ingredientProto = {
     }
 };
 
+const AllowedKeys = {
+    ARROW_LEFT:   'ArrowLeft',
+    ARROW_RIGHT:  'ArrowRight',
+    ARROW_UP:     'ArrowUp',
+    ARROW_DOWN:   'ArrowDown'
+}
+
 window.onload = () => {
+    // Initiating Canvas
+    window.customElements.define('pingpong-table', Grandstand, { extends: "canvas"});
+
+    globalThis.grandstand = document.querySelector("canvas[is='pingpong-table']");
+
     let ingredients = Object.create(ingredientProto);
 
-    ingredients.position.X = 0;
-    ingredients.position.Y = 0;
-    ingredients.weight = 50;
-    ingredients.rectangle.height = 10;
-    ingredients.rectangle.width = 20;
-    ingredients.rectangle.color = "white";
+    ingredients.position.X        = 125;
+    ingredients.position.Y        = 135;
+    ingredients.weight            = 25;
+    ingredients.rectangle.height  = 2;
+    ingredients.rectangle.width   = 50;
+    ingredients.rectangle.color   = "#FF0000";
 
-    let paddle = new Paddle(ingredients);
-    console.log(paddle);
+    let racket = new Racket(ingredients);
+
+    grandstand.onkeydown = (event) => {
+        switch(event.code) {
+            case AllowedKeys.ARROW_LEFT:
+                setTimeout(() => {
+                    racket.moveLeft();
+                }, 10);
+                racket.clearAnimation();
+                break;
+            case AllowedKeys.ARROW_RIGHT:
+                setTimeout(() => {
+                    racket.moveRight();
+                }, 10);
+                racket.clearAnimation();
+                break;
+            case AllowedKeys.ARROW_UP:
+                console.log(Sense.Up);
+                break;
+            case AllowedKeys.ARROW_DOWN:
+                console.log(Sense.Down);
+                break;
+            default:
+                return;
+        }
+    }
 };
